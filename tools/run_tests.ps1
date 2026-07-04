3491594286f97d28a499f1c1e20b282b781ebd08
sxmxc <steven.mcneely@live.com>
2026-07-04 00:18:15 -0500

    ci fix

--- tools/run_tests.ps1
+++ tools/run_tests.ps1
@@ -1,1 +1,2 @@
+godot --headless --path . --import
 godot --headless --path . --log-file .godot/gut.log --script res://addons/gut/gut_cmdln.gd -gconfig=res://.gutconfig.json -gdir=res://tests -ginclude_subdirs -gexit -gexit_on_success
